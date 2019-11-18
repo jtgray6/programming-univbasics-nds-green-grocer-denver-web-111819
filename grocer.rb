e8251f81aadfa804832e27f4626a50de28acb97a
Jeremy Gray <jeremygray@utexas.edu>
2019-11-18 03:37:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,11 +20,11 @@ def consolidate_cart(cart)
   output = []
   cart.each do |item|
     item_name = item.keys[0]
-    if output[item_name]
-      output[item_name][:count] += 1 
+    if output
+      output[:count] += 1 
     else
-      output[item_name] = item[item_name]
-      output[item_name][:count] = 1 
+      output = item
+      output[:count] = 1 
     end
   end
   output
